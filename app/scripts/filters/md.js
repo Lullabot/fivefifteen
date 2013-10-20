44ea579973df79c55a33e2ef5f812f7b37ff6c24
Jerad Bitner <sirkitree@gmail.com>
2013-10-20 12:11:35 -0600

    gh-pages release branch

--- app/scripts/filters/md.js
+++
@@ -1,9 +0,0 @@
-angular.module('fivefifteenApp.filters', [])
-  .filter('markdown', function() {
-    return function(text, convert) {
-      if (typeof text !== 'undefined') {
-        var converted = marked(text);
-        return converted;
-      }
-    }
-  });
