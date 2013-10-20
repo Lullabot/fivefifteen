e18684502fc880b736c89ecc6078b2ab8f9e42a4
Jerad Bitner <sirkitree@gmail.com>
2013-10-20 12:13:02 -0600

    update

---
+++ scripts/directives/clock.js
@@ -0,0 +1,31 @@
+angular.module('fivefifteenApp.directives', [])
+  .directive('clock', function ($timeout, dateFilter, Data) {
+    return function (scope, element, attrs) {
+      var timeoutId; // timeoutId, so that we can cancel the time updates
+      var init = Data.initTime;
+      var current = new Date();
+      var elapsed = new Date();
+      elapsed.setTime(current.getTime() - init.getTime());
+      element.text("Minute/s elapsed: " + elapsed.getMinutes());
+      
+      // schedule update in one second
+      function updateLater() {
+        // save the timeoutId for canceling
+        timeoutId = $timeout(function() {
+          var current = new Date();
+          var elapsed = new Date();
+          elapsed.setTime(current.getTime() - init.getTime());
+          element.text("Minute/s elapsed: " + elapsed.getMinutes());
+          updateLater(); // schedule another update
+        }, 1000);
+      }
+        
+      // listen on DOM destroy (removal) event, and cancel the next UI update
+      // to prevent updating time ofter the DOM element was removed.
+      element.bind('$destroy', function() {
+        $timeout.cancel(timeoutId);
+      });
+ 
+      updateLater(); // kick off the UI update process.
+    }
+  });
