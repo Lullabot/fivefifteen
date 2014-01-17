1932a83320d4a982c83d3714808904be04dd3df3
James Sansbury <james.sansbury@lullabot.com>
2014-01-16 20:53:01 -0700

    Remove blockquotes from descriptions.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,22 +11,22 @@ angular.module('fivefifteenApp')
   
       'success' : {
           "title": "## Recent Successes",
-          "text": "> Gloat, it's good for ya. Note even the small successes. It's also very good Karma to mention the team members that helped."
+          "text": "Gloat, it's good for ya. Note even the small successes. It's also very good Karma to mention the team members that helped."
       },
 
       'learned' : {
           'title' : '## Things I’ve Learned Recently',
-          'text' : "> Keep in mind both professional AND personal things here. Here are some topics to help prime your thinking: Professional might include: Learned a new project methodology, deepened a core skill, evolved coding prowess, took a Coursera course, or other things related to your job role. Personal skills might cover: time management, associate mentorship, social skills, and company culture perspectives."
+          'text' : "Keep in mind both professional AND personal things here. Here are some topics to help prime your thinking: Professional might include: Learned a new project methodology, deepened a core skill, evolved coding prowess, took a Coursera course, or other things related to your job role. Personal skills might cover: time management, associate mentorship, social skills, and company culture perspectives."
       },
       
       'challenges' : {
           'title' : '## Current Challenges',
-          'text' : "> Everybody has 'em. Talking openly about challenges provides you the opportunity to get direct mentorship (something not to miss) from your manager or mentor and helps to clarify things for them to focus on. Course the goal is that items under “Current Challenges” move up to “Things I’ve Learned Recently.” "
+          'text' : "Everybody has 'em. Talking openly about challenges provides you the opportunity to get direct mentorship (something not to miss) from your manager or mentor and helps to clarify things for them to focus on. Course the goal is that items under “Current Challenges” move up to “Things I’ve Learned Recently.” "
       },
 
       'projects' : {
           'title' : '## Project Updates/Reviews',
-          'text' : "> This could be a status update on a particular project, or topics and concepts that come up in the context of projects, but live beyond the context of a specific client. Try answering these questions to get yourself started:"
+          'text' : "This could be a status update on a particular project, or topics and concepts that come up in the context of projects, but live beyond the context of a specific client. Try answering these questions to get yourself started:"
                   + "\n\n"
                       + "* Are your projects on time?\n"
                       + "* How is your workload?\n"
@@ -36,12 +36,12 @@ angular.module('fivefifteenApp')
 
       'objectives' : {
           'title' : '## Annual Development Objectives',
-          'text' : "> Identify skills you would like to develop or opportunities you would like to experience. This may not change very often, but it serves as a reminder to discuss opportunities to develop."
+          'text' : "Identify skills you would like to develop or opportunities you would like to experience. This may not change very often, but it serves as a reminder to discuss opportunities to develop."
       },
 
       'feedback' : {
           'title' : '## Feedback for Your Manager',
-          'text' : "> Time to provide some feedback to your manager. Again, be honest. Everybody can improve what they do. Honesty is the start."
+          'text' : "Time to provide some feedback to your manager. Again, be honest. Everybody can improve what they do. Honesty is the start."
       },
       
       url: 'views/steps.html'
