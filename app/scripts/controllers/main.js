54652980000a09a29ce84531e4543a635450a96f
James Sansbury <james.sansbury@lullabot.com>
2014-01-17 10:24:29 -0700

    Refactor various views to use data from db.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,59 +1,35 @@
 'use strict';
 
 angular.module('fivefifteenApp')
-  .controller('MainCtrl', function ($scope, Data) {
+  .controller('MainCtrl', function ($scope, $routeParams, Data, Steps) {
 
     // Simple Data service to persist form values.
     $scope.data = Data;
 
-    Data.ref.$on('loaded', function (values) {
-      var steps = values;
-      $scope.shebang = ""
-        + steps.success.title
-        + "\n"
-        + steps.success.text
-        + "\n\n"
-        + [Data.success === 'undefined' ? '' : Data.success]
-        + "\n\n"
-        + steps.learned.title
-        + "\n"
-        + steps.learned.text
-        + "\n\n"
-        + [Data.learned === 'undefined' ? '' : Data.learned]
-        + "\n\n"
-        + steps.challenges.title
-        + "\n"
-        + steps.challenges.text
-        + "\n\n"
-        + [Data.challenges === 'undefined' ? '' : Data.challenges]
-        + "\n\n"
-        + steps.projects.title
-        + "\n"
-        + steps.projects.text
-        + "\n\n"
-        + [Data.projects === 'undefined' ? '' : Data.projects]
-        + "\n\n"
-        + steps.objectives.title
-        + "\n"
-        + steps.objectives.text
-        + "\n\n"
-        + [Data.objectives === 'undefined' ? '' : Data.objectives]
-        + "\n\n"
-        + steps.feedback.title
-        + "\n"
-        + steps.feedback.text
-        + "\n\n"
-        + [Data.feedback === 'undefined' ? '' : Data.feedback]
-        + "\n\n"
-        + "";
+    $scope.path = $routeParams.stepName;
+
+    $scope.steps = Steps.data;
+
+    Steps.promise.$on('loaded', function(values) {
+      // If we get values, store it both in the scope and Steps.
+      if (typeof values !== "undefined") {
+        angular.extend(Steps.data, values);
+      }
     });
   })
 
   // scope data is not persistent across views so we use a simple service.
-  .factory('Data', function ($firebase) {
+  .factory('Data', function () {
     // This variable holds all of the text used on the site.
-    var url = new Firebase("https://fivefifteen.firebaseio.com/steps");
+    return {};
+  })
+
+  .factory('Steps', function($firebase) {
+    var url = new Firebase("https://fivefifteen.firebaseio.com/steps"),
+        ref = $firebase(url);
+
     return {
-      ref: $firebase(url)
+      "promise": ref,
+      "data": {}
     };
   });
